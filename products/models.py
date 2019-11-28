a0367b733bc44d9dd2d9c54b14f14eb7103e33d2
nezouse <stankiewicz1998@gmail.com>
2019-11-28 23:56:40 +0100

    Add more validation to userProduct model

--- products/models.py
+++ products/models.py
@@ -14,8 +14,18 @@ class Product(models.Model):
 
 
 class UserProduct(models.Model):
+    mealtime_choices = (
+        ('breakfast', 'Breakfast'),
+        ('lunch', 'Lunch'),
+        ('dinner', 'Dinner'),
+        ('supper', 'Supper')
+    )
+
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    mealtime = models.CharField(max_length=20)
+    mealtime = models.CharField(max_length=10, choices=mealtime_choices)
     date = models.DateField()
     weight = models.IntegerField()
+
+    class Meta:
+        unique_together = ('product', 'date', 'mealtime')
