92b0cf2e4d3629d6bd24260655df08f12eda297d
nezouse <stankiewicz1998@gmail.com>
2019-12-03 20:37:08 +0100

    Add drf auth endpoints

--- fitit/urls.py
+++ fitit/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('products/', include('products.urls')),
-    path('users/', include('users.urls'))
+    path('users/', include('users.urls')),
+    path('api_auth/', include('rest_framework.urls'))
 ]
