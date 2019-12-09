f3676a1f0e551f9ac48b74dada8aee82d4ce26a7
nezouse <stankiewicz1998@gmail.com>
2019-12-09 21:26:37 +0100

    Rename serializer field to camelCase

--- users/serializers.py
+++ users/serializers.py
@@ -3,14 +3,14 @@ from rest_framework import serializers
 
 
 class UserSerializer(serializers.ModelSerializer):
-    confirm_password = serializers.CharField(write_only=True)
+    confirmPassword = serializers.CharField(write_only=True)
     password = serializers.CharField()
 
     def validate(self, data):
-        if data["confirm_password"] != data["password"]:
+        if data["confirmPassword"] != data["password"]:
             raise serializers.ValidationError("Passwords don't match")
         return data
 
     class Meta:
         model = User
-        fields = ['email', 'username', 'password', 'confirm_password']
+        fields = ['email', 'username', 'password', 'confirmPassword']
